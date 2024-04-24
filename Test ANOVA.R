b753df61a36f90030a0b8eb9c26fcbe372c4f38f
Rayan <160314349+RAYCTX2005@users.noreply.github.com>
2024-04-24 08:28:29 +0000

    Update Test ANOVA.R

--- Test ANOVA.R
+++ Test ANOVA.R
@@ -26,4 +26,42 @@ library(car)
 qqPlot(res_aov$residuals,
   id = FALSE # id = FALSE to remove point identification
 )
- shapiro.test(res_aov$residuals)        
+ shapiro.test(res_aov$residuals)  
+# égalité des variance
+        # Boxplot
+boxplot(flipper_length_mm ~ species,
+  data = dat
+)
+         # test de levene
+library(car)
+
+leveneTest(flipper_length_mm ~ species,
+  data = dat
+)
+
+## Levene's Test for Homogeneity of Variance (center = median)
+##        Df F value Pr(>F)
+## group   2  0.3306 0.7188
+##       339
+
+    #valeur abérante
+  boxplot(flipper_length_mm ~ species,
+  data = dat
+)
+  #anova
+         # 1st method:
+oneway.test(flipper_length_mm ~ species,
+  data = dat,
+  var.equal = TRUE # assuming equal variances
+)
+
+## 
+## 	One-way analysis of means
+## 
+## data:  flipper_length_mm and species
+## F = 594.8, num df = 2, denom df = 339, p-value < 2.2e-16
+         
+        
+
+
+         
